b43758f7cd2b8b0255235bd7d0deed2b3e280329
MoseslGit <78578546+MoseslGit@users.noreply.github.com>
2022-12-18 22:09:08 +0000

    Update strategies.py
    Added functions to pass back to main

--- strategies.py
+++ strategies.py
@@ -1,3 +1,5 @@
+
+
 # Define function to execute trading strategy for crisis market condition
 def crisis_strategy(data):
     # Implement crisis trading strategy here and return suitable basket of assets
@@ -21,3 +23,46 @@ def woi_strategy(data):
     # Implement Walking on Ice trading strategy here and return suitable basket of assets
     basket = []
     return basket
+
+
+#Call this function from main to update portfolio depending on market conditions
+def update(portfolio, data, risk_free_rate, thresholds, frequency):
+    # Identify market condition and calculate probabilities
+    probabilities = train_model.identify_market_condition(data)
+
+    # Construct aggregated portfolio
+    portfolio = construct_portfolio(data, risk_free_rate, thresholds, probabilities)
+    
+    return portfolio
+
+def construct_portfolio(data, risk_free_rate, thresholds, probabilities):
+    # Initialize empty list to store asset baskets
+    baskets = []
+    
+    # Get asset baskets for each market condition
+    steady_state_basket = steady_state(data, risk_free_rate, thresholds)
+    inflation_basket = inflation(data, risk_free_rate, thresholds)
+    crisis_basket = crisis(data, risk_free_rate, thresholds)
+    walking_on_ice_basket = walking_on_ice(data, risk_free_rate, thresholds)
+    
+    # Append asset baskets to list
+    baskets.append(steady_state_basket)
+    baskets.append(inflation_basket)
+    baskets.append(crisis_basket)
+    baskets.append(walking_on_ice_basket)
+    
+    # Calculate asset weights for each basket
+    weights = probabilities / np.sum(probabilities)
+    
+    # Initialize empty list to store asset weights for each basket
+    basket_weights = []
+    
+    # Calculate asset weights for each basket
+    for basket in baskets:
+        basket_weight = basket * weights
+        basket_weights.append(basket_weight)
+    
+    # Calculate aggregated portfolio
+    aggregated_portfolio = np.sum(basket_weights, axis=0)
+    
+    return aggregated_portfolio

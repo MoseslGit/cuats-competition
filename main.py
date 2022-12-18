a7712a3e3310d3486fff17a79397ac8fac9a0c49
MoseslGit <78578546+MoseslGit@users.noreply.github.com>
2022-12-18 22:02:53 +0000

    Update main.py
    Changed to work with Quantconnect

--- main.py
+++ main.py
@@ -1,55 +1,103 @@
+from quantconnect.algorithm import QCAlgorithm
+from quantconnect.data import Market
+from quantconnect.python import (
+    HistoryRequest,
+    PortfolioTarget,
+    OrderDirection,
+    OrderType
+)
+import numpy as np
+from datetime import datetime
+
 import train_model
 import strategies
 import feedback
 
-def identify_market_condition(data):
-    # Fit Gaussian mixture model to data
-    model = GaussianMixture(n_components=4)
-    model.fit(data)
+class TradingStrategy(QCAlgorithm):
+    def Initialize(self):
+        # Set frequency of portfolio adjustments (e.g. daily, weekly, monthly)
+        frequency = 'daily'
+        
+        # Set start and end dates for backtesting
+        start_date = datetime(2020, 1, 1)
+        end_date = datetime(2020, 12, 31)
+        
+        # Set risk-free rate and threshold parameters
+        self.risk_free_rate = 0.05
+        self.thresholds = {
+            'risk_factor': 2.0,
+            'return_factor': 1.5,
+            'diversification_factor': 0.5,
+            'short_window': 20,
+            'long_window': 50
+        }
 
-    # Get probabilities of each market condition
-    market_condition_probs = model.predict_proba(data)
+        # Train machine learning model to identify market condition
+        self.model = train_model.train()
+        # Set up event handler for new data
+        self.SetWarmUp(self.Time.DaysInPeriod(frequency, start_date))
+        self.Schedule.On(self.DateRules.EveryDay(self.Symbols), self.TimeRules.AfterMarketOpen(self.Symbols), self.rebalance_portfolio)
+        # Set initial portfolio
+        self.portfolio = np.ones((self.Securities.Count,)) / self.Securities.Count
+        
+        # Set frequency for portfolio rebalancing
+        self.frequency = 1
+        
+        # Set flag to indicate first iteration
+        self.first_iteration = True
 
-    return market_condition_probs
-  
-# Define function to construct aggregated portfolio weighted to each market condition
-def construct_aggregated_portfolio(market_condition_probs, data):
-    # Initialize portfolio
-    portfolio = np.zeros((data.shape[1],))
+        # Subscribe to data for all securities
+        for symbol in self.SymbolCache:
+            self.AddData(symbol.ID.Symbol, symbol.ID.SecurityType, symbol.ID.Resolution)
+        
+    def OnData(self, data):
+        if self.first_iteration:
+            # Get historical data for all securities
+            history = self.History([HistoryRequest(symbol.ID, self.Time - self.frequency, self.Time, Resolution.Daily) for symbol in self.SymbolCache])
+            self.first_iteration = False
+        else:
+            # Update portfolio
+            market_condition = train_model.identify_market_condition(data)
+            self.portfolio = strategies.update(self.portfolio, data, market_condition)
 
-    # Get portfolios for each market condition
-    crisis_portfolio = crisis_trading_strategy(data)
-    steady_state_portfolio = steady_state_trading_strategy(data)
-    inflation_portfolio = inflation_trading_strategy(data)
-    walking_on_ice_portfolio = walking_on_ice_trading_strategy(data)
+            # Check if it is time to rebalance portfolio
+            if self.Time.day % self.frequency == 0:
+                # Rebalance portfolio
+                self.portfolio = feedback.adjust(self.portfolio, data, self.risk_free_rate, self.thresholds)
+                
+                # Execute trades to adjust portfolio
+                for i, symbol in enumerate(self.SymbolCache):
+                    asset_weight = self.portfolio[i]  # weight of asset in portfolio
+                    asset_holdings = self.Portfolio[symbol].Quantity  # current holdings of asset
 
-    # Aggregate portfolios
-    for i in range(data.shape[1]):
-        portfolio[i] = market_condition_probs[0][i] * crisis_portfolio[i] + market_condition_probs[1][i] * steady_state_portfolio[i] + market_condition_probs[2][i] * inflation_portfolio[i] + market_condition_probs[3][i] * walking_on_ice_portfolio[i]
+                    # Calculate number of shares to buy or sell
+                    shares = int(asset_weight * self.Portfolio.TotalPortfolioValue / data[symbol].Price) - asset_holdings
 
-    return portfolio
-  
-  
-  # Define function to update portfolio
-def update_portfolio(portfolio, data, risk_free_rate, thresholds, frequency):
-    # Identify current market condition
-    market_condition_probs = identify_market_condition(data)
+                    # Check if we need to buy or sell
+                    if shares > 0:
+                        # Buy shares
+                        self.Buy(symbol, shares)
+                    elif shares < 0:
+                        # Sell shares
+                        self.Sell(symbol, -shares)
+                    if shares == 0:
+                        continue  # skip to next asset
 
-    # Construct aggregated portfolio weighted to each market condition
-    portfolio = construct_aggregated_portfolio(market_condition)
-    return portfolio
-  
-  
-  # Set initial portfolio
-portfolio = np.ones((data.shape[1],)) / data.shape[1]
+                    if not self.Securities[symbol].IsTradable:
+                        self.Log(f'{symbol} is not tradable')
+                        continue  # skip to next asset
+                    try:
+                        # Buy shares
+                        self.Buy(symbol, shares)
+                    except Exception as e:
+                        self.Log(f'Error executing trade for {symbol}: {e}')
+                    # Log trade details
+                    self.Log(f'Traded {shares} shares of {symbol} at {data[symbol].Price:.2f}')
+                    self.Log(f'Updated portfolio weights: {self.portfolio}')
+                    # Update portfolio value and cash balance
+                    self.Portfolio.SetCash(self.Portfolio.Cash + shares * data[symbol].Price)
+                    self.Portfolio.SetHoldings(symbol, asset_weight, self.Portfolio.TotalPortfolioValue)
 
-# Set frequency for portfolio rebalancing
-frequency = 5
 
-# Iterate through time steps
-for i in range(1, data.shape[0]):
-    # Update portfolio
-    portfolio = update_portfolio(portfolio, data[i-1:i+1,:], risk_free_rate, thresholds, frequency)
 
-# Print final portfolio
-print(portfolio)
+                        

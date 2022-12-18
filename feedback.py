30f2b4a533b32e0d40cfb0a7cb44c492cec92229
MoseslGit <78578546+MoseslGit@users.noreply.github.com>
2022-12-18 22:04:50 +0000

    Update feedback.py

--- feedback.py
+++ feedback.py
@@ -1,99 +1,112 @@
-# MACD values
-def calculate_macd(data, short_window, long_window):
-    # Calculate short-term moving averages
-    short_moving_averages = data.rolling(window=short_window, center=False).mean()
+import numpy as np
 
-    # Calculate long-term moving averages
-    long_moving_averages = data.rolling(window=long_window, center=False).mean()
-
-    # Calculate MACD values
-    macd_values = short_moving_averages - long_moving_averages
-
-    return macd_values
-# Sharpe ratio
-def calculate_sharpe_ratio(portfolio, data, risk_free_rate):
-    returns = np.sum(portfolio * data)
-    std = np.std(portfolio * data)
-    sharpe_ratio = (returns - risk_free_rate) / std
-    return sharpe_ratio
-  
-  # Calculate threshold levels based on asset risk
-def calculate_risk_thresholds(data, risk_factor):
-    asset_risks = []
-    for i in range(data.shape[1]):
-        asset_risks.append(np.std(data[:,i]) * risk_factor)
-    return asset_risks
-
-# Calculate threshold levels based on asset return potential
-def calculate_return_thresholds(data, return_factor):
-    asset_returns = []
-    for i in range(data.shape[1]):
-        asset_returns.append(np.mean(data[:,i]) * return_factor)
-    return asset_returns
-
-# Calculate threshold levels based on asset diversification benefits
-def calculate_diversification_thresholds(data, diversification_factor):
-    asset_correlations = np.corrcoef(data)
-    asset_diversification = 1.0 / (1.0 + asset_correlations)
-    asset_diversification = np.mean(asset_diversification, axis=1)
-    asset_diversification *= diversification_factor
-    return asset_diversification
-  
-  # Define function to adjust portfolio based on Sharpe ratio and MACD values
-# Define function to adjust portfolio based on Sharpe ratio, MACD, and threshold values
-def adjust_portfolio(portfolio, data, risk_free_rate, thresholds):
+def adjust(portfolio, data, risk_free_rate, thresholds):
+    # Calculate risk for each asset
+    risks = calculate_risk(data)
+    
+    # Calculate return potential for each asset
+    returns = calculate_return(data)
+    
+    # Calculate diversification benefits for each asset
+    diversification = calculate_diversification(data)
+    
+    # Calculate Sharpe ratio for each asset
+    sharpe_ratios = (returns - risk_free_rate) / risks
+    
+    # Calculate threshold Sharpe ratio value for each asset
+    threshold_sharpe_ratios = (thresholds['risk_factor'] * risks) + (thresholds['return_factor'] * returns) + (thresholds['diversification_factor'] * diversification)
+    
+    # Initialize empty list to store new weights
+    new_weights = []
+    
+    # Calculate new weights for each asset
+    for i, symbol in enumerate(data.Keys):
+        if sharpe_ratios[i] >= threshold_sharpe_ratios[i]:
+            new_weight = 1 / len(data.Keys)  # equally weight all assets
+        else:
+            new_weight = 0
+            
+        new_weights.append(new_weight)
+    
+    # Normalize new weights to sum to 1
+    new_weights = new_weights / np.sum(new_weights)
+    
+    # Update portfolio
+    portfolio = new_weights
     # Calculate MACD for each asset
-    asset_macds = []
-    for i in range(data.shape[1]):
-        asset_macds.append(calculate_macd(data[:,i]))
+    macd_values = []
+    for symbol in data.Keys:
+        prices = data[symbol].Prices
+        macd = calculate_macd(prices, thresholds['short_window'], thresholds['long_window'])
+        macd_values.append(macd)
+
+    # Initialize empty list to store MACD weights
+    macd_weights = []
+
+    # Calculate MACD weights for each asset
+    for i, symbol in enumerate(data.Keys):
+        if macd_values[i] > 0:
+            macd_weight = 1 / len(data.Keys)  # equally weight all assets
+        else:
+            macd_weight = 0
+        macd_weights.append(macd_weight)
+
+    # Normalize MACD weights to sum to 1
+    macd_weights = macd_weights / np.sum(macd_weights)
+
+    # Combine Sharpe ratio weights and MACD weights
+    combined_weights = (portfolio + macd_weights) / 2
+
+    # Update portfolio
+    portfolio = combined_weights
+
+    return portfolio
+
+def calculate_risk(data):
+    # Initialize empty list to store risks
+    risks = []
+    
+    # Calculate standard deviation of returns for each asset
+    for symbol in data.Keys:
+        returns = data[symbol].Returns
+        risk = np.std(returns)
+        risks.append(risk)
         
-    # Identify assets with positive MACD
-    uptrend_assets = np.where(np.array(asset_macds) > 0)[0]
-    
-    # Increase exposure to assets in uptrend
-    portfolio[:,uptrend_assets] *= 1.2
-    
-    # Calculate individual asset Sharpe ratios
-    asset_sharpe_ratios = []
-    for i in range(data.shape[1]):
-        asset_sharpe_ratios.append(calculate_sharpe_ratio(portfolio[:,i], data[:,i], risk_free_rate))
-    
-    # Identify underperforming assets
-    underperformers = np.where(np.array(asset_sharpe_ratios) < thresholds)[0]
-    
-    # Reduce exposure to underperforming assets
-    portfolio[:,underperformers] *= 0.8
-    
-    # Rebalance portfolio to maintain target
-# Define main function to execute portfolio management system
-def manage_portfolio(portfolio, data, risk_free_rate, thresholds):
-    # Adjust portfolio based on Sharpe ratio, MACD, and threshold values
-    portfolio = adjust_portfolio(portfolio, data, risk_free_rate, thresholds)
-    
-    # Rebalance portfolio to maintain target allocation
-    portfolio /= np.sum(portfolio, axis=1, keepdims=True)
-    
-    return portfolio
-  
-def rebalance_portfolio(portfolio, data, risk_free_rate, thresholds, frequency):
-    # Check if it is time to rebalance portfolio
-    if data.shape[0] % frequency == 0:
-        # Rebalance portfolio
-        portfolio = adjust_portfolio(portfolio, data, risk_free_rate, thresholds)
+    return risks
 
-    # Return rebalanced portfolio
-    return portfolio
+def calculate_return(data):
+    # Initialize empty list to store returns
+    returns = []
+    
+    # Calculate mean of returns for each asset
+    for symbol in data.Keys:
+        asset_returns = data[symbol].Returns
+        mean_return = np.mean(asset_returns)
+        returns.append(mean_return)
+        
+    return returns
 
-  
-# Set initial portfolio and target allocation
-initial_portfolio = np.array([[0.25, 0.25, 0.25, 0.25]])
-target_allocation = initial_portfolio / np.sum(initial_portfolio, axis=1, keepdims=True)
+def calculate_diversification(data):
+    # Initialize empty list to store diversification benefits
+    diversification = []
+    
+    # Calculate diversification benefits for each asset
+    for symbol in data.Keys:
+        # Calculate pairwise correlations between asset and all other assets
+        correlations = []
+        for compare_symbol in data.Keys:
+            if symbol != compare_symbol:
+                asset_returns = data[symbol].Returns
+                compare_asset_returns = data[compare_symbol].Returns
+                correlation = np.corrcoef(asset_returns, compare_asset_returns)[0][1]
+                correlations.append(correlation)
+        
+        # Calculate average pairwise correlation
+        avg_correlation = np.mean(correlations)
+        
+        # Calculate diversification benefit
+        diversification_benefit = 1 - avg_correlation
+        diversification.append(diversification_benefit)
+        
+    return diversification
 
-# Set risk-free rate and threshold values for different assets
-risk_free_rate = 0.05
-thresholds = {
-    'asset1': 0.15,
-    'asset2': 0.10,
-    'asset3': 0.05,
-    'asset4': 0.20
-}

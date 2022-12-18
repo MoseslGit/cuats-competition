a3a4488ea7a1ec9d597a3360680c3ec96c8b804b
MoseslGit <78578546+MoseslGit@users.noreply.github.com>
2022-12-18 21:05:48 +0000

    Create strategies.py
    Sample code framework

---
+++ strategies.py
@@ -0,0 +1,23 @@
+# Define function to execute trading strategy for crisis market condition
+def crisis_strategy(data):
+    # Implement crisis trading strategy here and return suitable basket of assets
+    basket = []
+    return basket
+
+# Define function to execute trading strategy for steady state market condition
+def steady_state_strategy(data):
+    # Implement steady state trading strategy here and return suitable basket of assets
+    basket = []
+    return basket
+
+# Define function to execute trading strategy for inflation market condition
+def inflation_strategy(data):
+    # Implement inflation trading strategy here and return suitable basket of assets
+    basket = []
+    return basket
+
+# Define function to execute trading strategy for Walking on Ice market condition
+def woi_strategy(data):
+    # Implement Walking on Ice trading strategy here and return suitable basket of assets
+    basket = []
+    return basket

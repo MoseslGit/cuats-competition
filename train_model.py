22b0e3da6ad396ab35649a7361c9c2a34c49c66f
rageoveralostpenny <wu.cindyx@gmail.com>
2023-01-28 13:56:11 +0000

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn import mixture
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -45,28 +44,3 @@ if __name__ == '__main__':
   Ys = np.random.randint(0, 100, (5, 2))
   test_model = train(Xs)
   print(test_model.predict(Ys))
-  
-  # def preprocess_data(data_path):
-#   # import data from CSV file
-#   df = pd.read_csv(data_path)
-
-#   # drop null values
-#   df.dropna(inplace=True)
-
-#   # split data into training and test sets
-#   X_train, X_test, y_train, y_test = train_test_split(df.drop('target', axis=1), df['target'], test_size=0.2)
-
-#   return X_train, X_test, y_train, y_test
-
-# def main():
-#     # preprocess data
-#     X_train, X_test, y_train, y_test = preprocess_data('data.csv')
-
-#     # train model
-#     model = train(X_train, y_train)
-
-#     # identify market condition
-#     probabilities = identify_market_condition(X_test)
-
-#     # save model
-#     joblib.dump(model, 'model.joblib')
